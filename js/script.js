1ec73635ece39a7eeb87aec6dc346c4043282dce
ghericsantiago <ghericsantiago@gmail.com>
2017-04-12 20:41:04 +0800

    Added Repeat functionality Added Shuffle functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 function player()
 {
 	var self = this;
-	var app = $('#my-player')[0];
-
-	self.temp = [];
-	self.volume = ko.observable(app.volume * 100);
+	var app = new Audio();
+	self.temp = {};
+	self.volume = ko.observable(.09 * 100);
 	self.current = ko.observable();
 	self.playing = ko.observable(false);
 	self.currentTime = ko.observable(0);
@@ -14,9 +13,23 @@ function player()
 		{ id : 0 , url:'audio/gitara.mp3' , name : 'Gitara' },
 		{ id : 1 , url:'audio/harana.mp3' , name : 'Harana' },
 		{ id : 2 , url:'audio/harana.mp3' , name : 'Harana2' },
-		{ id : 3 , url:'audio/horse.mp3' , name : 'Horse' }
+		{ id : 3 , url:'audio/horse.mp3' , name : 'Horse' },
+		{ id : 4 , url:'audio/gitara.mp3' , name : 'Gitara1' },
+		{ id : 5 , url:'audio/harana.mp3' , name : 'Harana3' },
+		{ id : 6 , url:'audio/harana.mp3' , name : 'Harana4' },
+		{ id : 7 , url:'audio/horse.mp3' , name : '1' },
+		{ id : 8 , url:'audio/gitara.mp3' , name : '2' },
+		{ id : 9 , url:'audio/harana.mp3' , name : '3' },
+		{ id : 10 , url:'audio/harana.mp3' , name : '4' },
+		{ id : 11 , url:'audio/horse.mp3' , name : '5' },
+		{ id : 12 , url:'audio/gitara.mp3' , name : '6' },
+		{ id : 13, url:'audio/harana.mp3' , name : '7' },
+		{ id : 14 , url:'audio/harana.mp3' , name : '8' },
+		{ id : 15 , url:'audio/horse.mp3' , name : '9' },
 		]);
 	self.queue = ko.observableArray([]);
+	self.random = ko.observable(false);
+	self.repeat = ko.observable(false);
 
 	/*
 	 * START OF PLAYER METHODS
@@ -34,23 +47,47 @@ function player()
 	 }
 
 	self.next = function(){
-		
+
 		currentIndex = self.findIndexById(self.current().id);
 
-		/*
-		 * LOOP THE SONG IN THE QUEUE 
-		 * IF THE LAST SONG IS REACHED
-		 */
-		if(currentIndex < self.queue().length - 1 && self.queue().length != 1)
-			self.current(self.queue()[currentIndex + 1]);
+		if(self.random())
+		{
+			do
+			{
+				randomPosition = self.getRandomInt(0, self.queue().length - 1);
+				nextSong = self.queue()[randomPosition];
+			}
+			while($.inArray(nextSong.id, self.temp.played) > -1 && self.queue().length != self.temp.played.length);
+			
+			if(self.queue().length != self.temp.played.length)
+				self.current(nextSong);
+		}
 		else
-			self.current(self.queue()[0]);
+		{
+			/*
+			 * LOOP THE SONG IN THE QUEUE 
+			 * IF THE LAST SONG IS REACHED
+			 */
+			if(currentIndex < self.queue().length - 1 && self.queue().length != 1)
+				self.current(self.queue()[currentIndex + 1]);
+			else
+				self.current(self.queue()[0]);
+		}
 
-		// CLEAR THE PLAYER
-		self.stop();
-		app.currentTime = 0;
-		app.src = self.queue()[self.findIndexById(self.current().id)].url;
-		self.play();
+		if(self.queue().length == self.temp.played.length)
+		{
+			console.log('All music had been played');
+			self.stop();
+			self.temp.played = [];
+		}
+		else
+		{
+			// CLEAR THE PLAYER
+			self.stop();
+			app.currentTime = 0;
+			app.src = self.queue()[self.findIndexById(self.current().id)].url;
+			self.play();
+		}
 	};
 
 	self.prev = function(){
@@ -61,21 +98,47 @@ function player()
 		 * IF THE FIRST SONG IS REACHED
 		 */
 		currentIndex =  self.findIndexById(self.current().id);
-		if(currentIndex > 0)
+
+		if(self.random())
 		{
-			currentSong = self.queue()[currentIndex - 1];
-			self.current(currentSong);
+			do
+			{
+				randomPosition = self.getRandomInt(0, self.queue().length - 1);
+				nextSong = self.queue()[randomPosition];
+			}
+			while($.inArray(nextSong.id, self.temp.played) > -1 && self.queue().length != self.temp.played.length);
+			
+			if(self.queue().length != self.temp.played.length)
+				self.current(nextSong);
 		}
 		else
 		{
-			self.current(self.queue()[self.queue().length - 1]);
+			if(currentIndex > 0)
+			{
+				currentSong = self.queue()[currentIndex - 1];
+				self.current(currentSong);
+			}
+			else
+			{
+				self.current(self.queue()[self.queue().length - 1]);
+			}
 		}
 
-		// CLEAR THE PLAYER
-		self.stop();
-		app.currentTime = 0;
-		app.src = self.queue()[self.findIndexById(self.current().id)].url;
-		self.play();
+		
+		if(self.queue().length == self.temp.played.length)
+		{
+			console.log('All music had been played');
+			self.stop();
+			self.temp.played = [];
+		}
+		else
+		{
+			// CLEAR THE PLAYER
+			self.stop();
+			app.currentTime = 0;
+			app.src = self.queue()[self.findIndexById(self.current().id)].url;
+			self.play();
+		}
 	};
 
 	self.addToQueue = function(e){
@@ -176,8 +239,8 @@ function player()
 	};
 
 	self.stop = function(){
+		console.log('player stoped');
 
-		console.log('song is stop');
 		var waitTime = 150;
 		setTimeout(function () {  
 		  	self.playing(false);
@@ -185,12 +248,13 @@ function player()
 			self.currentTime(0);
 			app.currentTime = self.currentTime();
 			self.seekerPercentage(0);
+			self.bufferPercentage(0);
 		}, waitTime);	
 	};
 
 	self.pause = function()
 	{
-		console.log('song is paused');
+		console.log('player is paused');
 		var waitTime = 150;
 		setTimeout(function () {   
 		  if (self.playing() == true) {
@@ -209,7 +273,19 @@ function player()
 		var waitTime = 150;
 		setTimeout(function () {   
 			if (self.playing() == false) {
-			   self.playing(true);
+			    self.playing(true);
+			    
+			    // add the id of current song
+				// in played queue
+				key = self.current().id;
+				if(!self.temp.played)
+			    	self.temp.played = [];
+			    if($.inArray(key, self.temp.played) == -1)
+			    	self.temp.played.push(key);	
+
+			    if(self.random() == false)
+			    	self.temp.played = [];
+
 				app.currentTime = self.currentTime();
 				app.play();	
 			 }
@@ -286,11 +362,43 @@ function player()
 		
 	}
 
+	self.toggleRandom = function()
+	{
+		self.random((self.random()) ? false : true );
+
+		if(self.random() == true)
+		{
+			key = self.current().id;
+			if(!self.temp.played)
+		    	self.temp.played = [];
+		    if($.inArray(key, self.temp.played) == -1)
+		    	self.temp.played.push(key);	
+		}
+
+	};
+
+	self.toggleRepeat = function()
+	{
+		self.repeat((self.repeat()) ? false : true);
+	}
+
+
+	/*
+	 * HELPER
+	 */
+
+	 /**
+	 * Returns a random integer between min (inclusive) and max (inclusive)
+	 * Using Math.round() will give you a non-uniform distribution!
+	 */
+	self.getRandomInt = function (min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 
 	/*
 	 * START OF ALL COMPUTED LISTENERS
 	 */
-
 	 self.volumeUpdated = ko.computed(function(){
 	 	volume = self.volume();
 	 	app.volume = volume / 100;
@@ -340,22 +448,33 @@ function player()
 	 */
 	app.addEventListener("ended", function(){
 
-		/*
-		 * CHECK IF THE NEXT SONG
-		 * IS NOT THE CURRENT SONG
-		 * TO PREVENT LOOP IF ALL SONG
-		 * IS DONE PLAYING
-		 */
-		if(self.findIndexById(self.current().id) != (self.queue().length - 1) )
+		if(self.repeat())
 		{
 			self.playing(false);
-     		self.next();	
+			self.play();
 		}
 		else
 		{
-			//self.seekerPercentage(0);
-			self.playing(false);	
+			/*
+			 * CHECK IF THE NEXT SONG
+			 * IS NOT THE CURRENT SONG
+			 * TO PREVENT LOOP IF ALL SONG
+			 * IS DONE PLAYING
+			 */
+			if(self.findIndexById(self.current().id) != (self.queue().length - 1) || self.random())
+			{
+				self.playing(false);
+	     		self.next();	
+			}
+			else
+			{
+				console.log('All music has been played');
+				self.stop();
+				self.playing(false);	
+			}
 		}
+
+		
 	});
 
 	app.addEventListener("timeupdate",function(){
